0e5006f525db592b0022247881cd836a8129ff01
idmaping <id.maping@gmail.com>
2021-08-18 15:20:28 +0700

    18 agustus

--- environment.py
+++ environment.py
@@ -10,18 +10,16 @@ class ENVIRONMENT:
         
         self.TGT_X = 0
         self.TGT_Y = 0
-        self.TGT_THETA = 180
-        self.TGT_RHO = 40
+        self.TGT_THETA = 180 #gaperlu
+        self.TGT_RHO = 40 #gaperlu
         
-
         self.POS_THETA = 180
         self.POS_RHO = 40
         self.POS_X = (self.ENV_WIDTH/2) + self.POS_RHO * np.cos(np.radians(self.POS_THETA))
         self.POS_Y = (self.ENV_WIDTH/2) + self.POS_RHO * -np.sin(np.radians(self.POS_THETA))
         
-        
         self.DIM_ACTION = 2
-        self.DIM_STATE = 1
+        self.DIM_STATE = 3
         
         self.RL_STATE = [0]
         self.RL_REWARD = 0
@@ -29,62 +27,111 @@ class ENVIRONMENT:
     def getReward(self):
         self.POS_X = (self.ENV_WIDTH/2) + self.POS_RHO * np.cos(np.radians(self.POS_THETA))
         self.POS_Y = (self.ENV_WIDTH/2) + self.POS_RHO * -np.sin(np.radians(self.POS_THETA))
-        reward = -LA.norm([[self.POS_X,self.POS_Y],[self.TGT_X,self.TGT_X]])
+        reward = (-LA.norm([[self.POS_X,self.POS_Y],[self.TGT_X,self.TGT_X]]))/1 + 35
         return reward
+
+    def getState(self):
+        return (self.TGT_X,self.TGT_Y,self.POS_THETA)
+
+    def reset(self):
+        self.TGT_X = np.random.randint(self.ENV_WIDTH)
+        self.TGT_Y = np.random.randint(self.ENV_HEIGHT)
+        self.POS_THETA = np.random.randint(359)
+
 
     def step(self,action):
         self._POS_RHO = self.POS_RHO
         if action == 0:
-            self.POS_THETA+=10
+            self.POS_THETA+=5
 
         elif action == 1:
-            self.POS_THETA-=10
-    
+            self.POS_THETA-=5
+
+        if self.POS_THETA>=360:
+            self.POS_THETA = self.POS_THETA-360
+
+        if self.POS_THETA<0:
+            self.POS_THETA = 360 + self.POS_THETA
+
+        
+
     def show(self):
         temp = self.ENV_BACKGROUND.copy()
         temp = cv2.line(temp, (50,50), (int(self.POS_X),int(self.POS_Y)), (0,255,0), 1)
-        temp = cv2.circle(temp, (int(self.TGT_X),int(self.TGT_Y)), 2, (255,255,255), 2)
+        temp = cv2.circle(temp, (int(self.TGT_X),int(self.TGT_Y)), 1, (255,255,255), 2)
         temp = cv2.resize(temp, ((200,200)), interpolation = cv2.INTER_AREA)
         
         cv2.imshow('ENVIRONMENT',temp)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        #cv2.waitKey(0)
+        #cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     env = ENVIRONMENT()
     env.step(1)
-    print("st1",env.getReward())
+    print("st1",env.getReward(),env.getState())
     env.show()
 
     env.step(1)
-    print("st2",env.getReward())
+    print("st2",env.getReward(),env.getState())
     env.show()
     
     env.step(1)
-    print("st3",env.getReward())
+    print("st3",env.getReward(),env.getState())
     env.show()
 
     env.step(1)
-    print("st4",env.getReward())
+    print("st4",env.getReward(),env.getState())
     env.show()
 
     env.step(1)
-    print("st5",env.getReward())
+    print("st5",env.getReward(),env.getState())
     env.show()
 
     env.step(1)
-    print("st6",env.getReward())
+    print("st6",env.getReward(),env.getState())
     env.show()
 
     env.step(1)
-    print("st7",env.getReward())
+    print("st7",env.getReward(),env.getState())
     env.show()
 
     env.step(1)
-    print("st8",env.getReward())
+    print("st8",env.getReward(),env.getState())
     env.show()
 
     env.step(1)
-    print("st9",env.getReward())
+    print("st9",env.getReward(),env.getState())
+    env.show()
+
+    env.step(1)
+    print("st9",env.getReward(),env.getState())
+    env.show()
+
+    env.step(1)
+    print("st9",env.getReward(),env.getState())
+    env.show()
+
+    env.step(1)
+    print("st9",env.getReward(),env.getState())
+    env.show()
+
+    env.step(1)
+    print("st9",env.getReward(),env.getState())
+    env.show()
+
+    env.step(1)
+    print("st9",env.getReward(),env.getState())
+    env.show()
+
+    env.step(1)
+    print("st9",env.getReward(),env.getState())
+    env.show()
+
+    env.step(1)
+    print("st9",env.getReward(),env.getState())
+    env.show()
+
+    env.step(1)
+    print("st9",env.getReward(),env.getState())
     env.show()
     
---
+++ q-learning.py
@@ -0,0 +1,93 @@
+import numpy as np
+from numpy import linalg as LA
+from environment import ENVIRONMENT
+import matplotlib.pyplot as plt
+import pickle
+import time
+import cv2
+
+class QLEARNING:
+    def __init__(self):
+        self.MAX_EPISODE = 50000
+        self.MAX_STEP = 300
+        self.SHOW_EVERY_EPISODE = 5000
+        self.EPSILON_DECAY = 0.9998
+        self.EPSILON = 0.9
+        self.LR = 0.1
+        self.DISCOUNT = 0.95
+        self.start_q_table = None
+        self.env = ENVIRONMENT()
+        self.q_table = {}
+        self.SHOW = False
+
+    def init(self):
+        if self.start_q_table is None:
+            print("NO Q-TABLE IS LOADED")
+            print("CREATING Q-TABLE")
+            for i in range(0,self.env.ENV_WIDTH+1):
+                for ii in range(0,self.env.ENV_HEIGHT+1):
+                    for iii in range(0,360):
+                        self.q_table[(i,ii,iii)] = [np.random.uniform(-5, 0) for i in range(self.env.DIM_ACTION)]
+            print(self.q_table[(0,0,0)][0])
+            print("DONE CREATING Q-TABLE")
+        else:
+            with open(self.start_q_table, "rb") as f:
+                self.q_table = pickle.load(f)
+    
+    def learn(self):
+        self.init()
+        #print(self.q_table[self.env.getState()][0])
+        episode_rewards = []
+        for episode in range(self.MAX_EPISODE):
+            
+            episode_reward = 0
+            self.env.reset()
+
+            if episode % self.SHOW_EVERY_EPISODE == 0 and episode>0 :
+                self.SHOW = True
+            else:
+                self.SHOW = False
+
+            for i in range(self.MAX_STEP):
+                state = self.env.getState()
+                if np.random.random() > self.EPSILON:
+                    action = np.argmax(self.q_table[state])
+                else:
+                    action = np.random.randint(0, self.env.DIM_ACTION)                        
+                self.env.step(action)
+                _state = self.env.getState()
+                max_future_q = np.max(self.q_table[_state])
+                current_q = self.q_table[state][action]
+                reward = self.env.getReward()
+
+                if reward >0:
+                    new_q = reward
+                else:
+                    new_q = (1 - self.LR) * current_q + self.LR * (reward + self.DISCOUNT * max_future_q)
+                self.q_table[state][action] = new_q
+
+                episode_reward += reward
+
+                if self.SHOW:
+                    #print("EPISODE : ",episode,"    R : ",episode_reward)
+                    self.env.show()
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+
+            
+            episode_rewards.append(episode_reward)
+            self.EPSILON *= self.EPSILON_DECAY
+
+            print("EPISODE : ",episode,"    R : ",episode_reward)
+        
+        moving_avg = np.convolve(episode_rewards, np.ones((self.SHOW_EVERY_EPISODE,))/self.SHOW_EVERY_EPISODE, mode='valid')
+        plt.plot([i for i in range(len(moving_avg))], moving_avg)
+        plt.ylabel(f"Reward {self.SHOW_EVERY_EPISODE}ma")
+        plt.xlabel("episode #")
+        plt.show()
+
+
+
+if __name__ == '__main__':
+    ql=QLEARNING()
+    ql.learn()
